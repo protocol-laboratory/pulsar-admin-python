3a84d466e110ff00052ae77a44e97e76058a0734
ZhangJian He <shoothzj@gmail.com>
2023-03-24 13:18:28 +0800

    add create delete persistent topic (#3)

--- pulsar_admin/persistent_topics.py
+++ pulsar_admin/persistent_topics.py
@@ -1,3 +1,57 @@
+import json
+from typing import Dict
+
+from pulsar_admin.http_client import HttpClient
+
+from pulsar_admin.pulsar_admin_exception import PulsarAdminException
+from pulsar_admin.url_const import UrlConst
+
+
 class PersistentTopics:
-    def __init__(self, http_client):
+    def __init__(self, http_client: HttpClient):
         self.http_client = http_client
+
+    def create_partitioned_topic(self, tenant: str, namespace: str, topic: str, num_partitions: int,
+                                 create_local_topic_only: bool = False):
+        params = [("createLocalTopicOnly", str(create_local_topic_only))]
+        status_code, _ = self.http_client.put(
+            f"{UrlConst.PERSISTENT}/{tenant}/{namespace}/{topic}{UrlConst.PARTITIONS}",
+            json.dumps(num_partitions),
+            params=params
+        )
+        if status_code != 204:
+            raise PulsarAdminException(f"failed to create partitioned topic {tenant}/{namespace}/{topic}, "
+                                       f"status code {status_code}")
+
+    def delete_partitioned_topic(self, tenant: str, namespace: str, topic: str, force: bool = False,
+                                 authoritative: bool = False):
+        params = [("force", str(force)), ("authoritative", str(authoritative))]
+        status_code, _ = self.http_client.delete(
+            f"{UrlConst.PERSISTENT}/{tenant}/{namespace}/{topic}{UrlConst.PARTITIONS}",
+            params=params
+        )
+        if status_code != 204:
+            raise PulsarAdminException(f"failed to delete partitioned topic {tenant}/{namespace}/{topic}, "
+                                       f"status code {status_code}")
+
+    def create_topic(self, tenant: str, namespace: str, topic: str, authoritative: bool = False,
+                     properties: Dict[str, str] = None):
+        params = [("authoritative", str(authoritative))]
+        status_code, _ = self.http_client.put(
+            f"{UrlConst.PERSISTENT}/{tenant}/{namespace}/{topic}",
+            json.dumps(properties) if properties else None,
+            params=params
+        )
+        if status_code != 204:
+            raise PulsarAdminException(f"failed to create non-partitioned topic {tenant}/{namespace}/{topic}, "
+                                       f"status code {status_code}")
+
+    def delete_topic(self, tenant: str, namespace: str, topic: str, force: bool = False, authoritative: bool = False):
+        params = [("force", str(force)), ("authoritative", str(authoritative))]
+        status_code, _ = self.http_client.delete(
+            f"{UrlConst.PERSISTENT}/{tenant}/{namespace}/{topic}",
+            params=params
+        )
+        if status_code != 204:
+            raise PulsarAdminException(f"failed to delete non-partitioned topic {tenant}/{namespace}/{topic}, "
+                                       f"status code {status_code}")
---
+++ pulsar_admin/persistent_topics_test.py
@@ -0,0 +1,48 @@
+import unittest
+
+from pulsar_admin.p_admin import PulsarAdmin
+from pulsar_admin.random_util import RandomUtil
+from pulsar_admin.tenant_info import TenantInfo
+
+
+class TestPersistentTopics(unittest.TestCase):
+    def setUp(self):
+        self.admin = PulsarAdmin('localhost', 8080)
+
+    def tearDown(self):
+        pass
+
+    def test_create_and_delete_topic(self):
+        tenant = RandomUtil.random_string()
+        namespace = RandomUtil.random_string()
+        topic = RandomUtil.random_string()
+        tenant_info = TenantInfo(admin_roles=set(), allowed_clusters={'standalone'})
+        self.admin.tenants.create_tenant(tenant, tenant_info.to_json())
+        self.admin.namespaces.create_namespace(tenant, namespace)
+        # Create a non-partitioned topic
+        self.admin.persistent_topics.create_topic(tenant, namespace, topic, False, {})
+
+        # Delete the topic
+        self.admin.persistent_topics.delete_topic(tenant, namespace, topic, False, False)
+        self.admin.namespaces.delete_namespace(tenant, namespace)
+        self.admin.tenants.delete_tenant(tenant, False)
+
+    def test_create_and_delete_partitioned_topic(self):
+        tenant = RandomUtil.random_string()
+        namespace = RandomUtil.random_string()
+        topic = RandomUtil.random_string()
+        tenant_info = TenantInfo(admin_roles=set(), allowed_clusters={'standalone'})
+        self.admin.tenants.create_tenant(tenant, tenant_info.to_json())
+        self.admin.namespaces.create_namespace(tenant, namespace)
+        # Create a partitioned topic
+        num_partitions = 3
+        self.admin.persistent_topics.create_partitioned_topic(tenant, namespace, topic, num_partitions, False)
+
+        # Delete the partitioned topic
+        self.admin.persistent_topics.delete_partitioned_topic(tenant, namespace, topic, False, False)
+        self.admin.namespaces.delete_namespace(tenant, namespace)
+        self.admin.tenants.delete_tenant(tenant, False)
+
+
+if __name__ == "__main__":
+    unittest.main()
